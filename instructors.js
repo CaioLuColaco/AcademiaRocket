ed44b2eef9f36c93327a4ac1f7360fff53e3793a
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-11-23 16:51:12 -0300

    organizated the data

--- instructors.js
+++ instructors.js
@@ -11,10 +11,21 @@ exports.post = function(req, res) {
         }
     }
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    let { avatar_url, birth, name, services, gender} = req.body
 
-    data.instructors.push(req.body)
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("write file")
