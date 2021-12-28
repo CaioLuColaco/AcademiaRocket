c291e694a14dbcec41e52025f4e1e99be69dc8d3
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-12-28 13:00:10 -0300

    initialized the database

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
 const { age, date } = require("../../lib/utils")
+const db = require('../../config/db')
 
 module.exports = {
     index(req, res) {
@@ -16,9 +17,33 @@ module.exports = {
             }
         }
     
-        let { avatar_url, birth, name, services, gender} = req.body
-    
-        return
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING id 
+        `
+
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).iso,
+            date(Date.now()).iso,
+        ]
+
+        db.query(query, values, function(err, results) {
+            console.log(err)
+            console.log(results)
+            return
+        })
+        
     },
     show(req, res) {
         return
