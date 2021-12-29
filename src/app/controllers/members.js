c8839571871c59a2dc0d379f5dc0a3cbdd08fbf7
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-12-29 13:24:35 -0300

    update member's database configuration

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,12 +1,18 @@
 const { age, date } = require("../../lib/utils")
+const Member = require("../models/Member")
 
 module.exports = {
     index(req, res) {
-        return res.render("members/index")
+
+        Member.all(function(members) {
+            return res.render("members/index", { members })
+        })
     },
+
     create(req, res) {
         return res.render("members/create")
     },
+
     post(req, res) {
         const keys = Object.keys(req.body)
 
@@ -15,15 +21,33 @@ module.exports = {
                 return res.send('please, fill')
             }
         }
+    
+        Member.create(req.body, function(member) {
+            return res.redirect(`/members/${member.id}`)
+        })
         
-        return
     },
+    
     show(req, res) {
-        return
+        Member.find(req.params.id, function(member) {
+            if (!member) return res.send("Member not found!")
+
+            member.birth = date(member.birth).birthDay
+
+            return res.render("members/show", { member })
+        })
     },
+
     edit(req, res) {
-        return
+        Member.find(req.params.id, function(member) {
+            if (!member) return res.send("Member not found!")
+
+            member.birth = date(member.birth).iso
+
+            return res.render("members/edit", { member })
+        })
     },
+
     put(req, res) {
         const keys = Object.keys(req.body)
 
@@ -33,9 +57,14 @@ module.exports = {
             }
         }
 
-        return
+        Member.update(req.body, function() {
+            return res.redirect(`/members/${req.body.id}`)
+        })
     },
+
     delete(req, res) {
-        return
+        Member.delete(req.body.id, function() {
+            return res.redirect(`/members`)
+        })
     }
 }
