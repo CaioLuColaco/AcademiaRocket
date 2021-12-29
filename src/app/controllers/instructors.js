edb26650051e5647772d4ebfa44580ea70f8c8fc
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-12-29 11:17:01 -0300

    finalized the migration of data with instructors to database

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,13 +1,18 @@
 const { age, date } = require("../../lib/utils")
-const db = require('../../config/db')
+const Instructor = require("../models/Instructor")
 
 module.exports = {
     index(req, res) {
-        return res.render("instructors/index", { instructors: data.instructors})
+
+        Instructor.all(function(instructors) {
+            return res.render("instructors/index", { instructors })
+        })
     },
+
     create(req, res) {
         return res.render("instructors/create")
     },
+
     post(req, res) {
         const keys = Object.keys(req.body)
 
@@ -17,40 +22,33 @@ module.exports = {
             }
         }
     
-        const query = `
-            INSERT INTO instructors (
-                name,
-                avatar_url,
-                gender,
-                services,
-                birth,
-                created_at
-            ) VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING id 
-        `
-
-        const values = [
-            req.body.name,
-            req.body.avatar_url,
-            req.body.gender,
-            req.body.services,
-            date(req.body.birth).iso,
-            date(Date.now()).iso,
-        ]
-
-        db.query(query, values, function(err, results) {
-            console.log(err)
-            console.log(results)
-            return
+        Instructor.create(req.body, function(instructor) {
+            return res.redirect(`/instructors/${instructor.id}`)
         })
         
     },
     show(req, res) {
-        return
+        Instructor.find(req.params.id, function(instructor) {
+            if (!instructor) return res.send("Instructor not found!")
+
+            instructor.age = age(instructor.birth)
+            instructor.services = instructor.services.split(",")
+            instructor.created_at = date(instructor.created_at).format
+
+            return res.render("instructors/show", { instructor })
+        })
     },
+
     edit(req, res) {
-        return
+        Instructor.find(req.params.id, function(instructor) {
+            if (!instructor) return res.send("Instructor not found!")
+
+            instructor.birth = age(instructor.birth).iso
+
+            return res.render("instructors/edit", { instructor })
+        })
     },
+
     put(req, res) {
         const keys = Object.keys(req.body)
 
@@ -60,9 +58,14 @@ module.exports = {
             }
         }
 
-        return
+        Instructor.update(req.body, function() {
+            return res.redirect(`/instructors/${req.body.id}`)
+        })
     },
+
     delete(req, res) {
-        return
+        Instructor.delete(req.body.id, function() {
+            return res.redirect(`/instructors`)
+        })
     }
 }
