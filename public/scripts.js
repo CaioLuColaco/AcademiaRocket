fefec7fbb1db2d480cdb3f05dc8562e74866b163
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2022-01-05 17:33:44 -0300

    finalized the structure of pagination

--- public/scripts.js
+++ public/scripts.js
@@ -15,3 +15,31 @@ const formDelete = document.querySelector("#form-delete")
             event.preventDefault()
         }
     })
+
+function pagination(selectedPage, totalPages) {
+    let pages = [],
+        oldPage
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...")
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
+        }
+    }
+
+    return pages
+}
--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -15,21 +15,17 @@ module.exports = {
             limit,
             offset,
             callback(instructors) {
-                return res.render("instructors/index", { instructors, filter})
+
+                const pagination = {
+                    total: Math.ceil(instructors[0].total / limit),
+                    page
+                }
+
+                return res.render("instructors/index", { instructors, pagination, filter})
             }
         }
 
         Instructor.paginate(params)
-
-        // if ( filter ) {
-        //     Instructor.findBy(filter, function(instructors) {
-        //         return res.render("instructors/index", { instructors, filter })
-        //     })
-        // } else {
-        //     Instructor.all(function(instructors) {
-        //         return res.render("instructors/index", { instructors })
-        //     })
-        // }
 
     },
 
